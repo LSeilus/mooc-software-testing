68e767c2142e1483aec903942b313b9ed5211b7c
LSeilus <lseilus@tutanota.com>
2021-10-10 13:52:04 +0200

    caesar cipher bugtesting done

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,32 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
+    @ParameterizedTest
+    @CsvSource({"@$(^:, 2, invalid", "|>/, 0, invalid"})
+    public void nonLetters(String text, int shift, String result){
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        String msg = cipher.CaesarShiftCipher(text, shift);
+        Assertions.assertEquals(result, msg);
+    }
+    @ParameterizedTest
+    @CsvSource({"abc"})
+    public void noShift(String text){
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        String msg = cipher.CaesarShiftCipher(text, 0);
+        Assertions.assertEquals(text, msg);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"xyz, abc, 3", "yz, za, 1"})
+    public void wraparound(String text, String result, int shift){
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        String msg = cipher.CaesarShiftCipher(text, shift);
+        Assertions.assertEquals(result, msg);
+    }
 
 }
